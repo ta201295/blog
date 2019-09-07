6a7bf9476ec2bb3aff9047dcdbfec9a8208b05ba
unknown <daotuananh201295>
2019-09-07 14:27:27 +0700

    part6

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,92 +1,28 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+@extends('main')
+    
+@section('content')    
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Contact Me</h1>
+            <hr>
+            <form>
+                <div class="from-group">
+                    <label name="email">Email:</label>
+                    <input id="email" name="email" class="form-control">
                 </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Contact Me
+                <div class="from-group">
+                    <label name="subject">Subject:</label>
+                    <input id="subject" name="subject" class="form-control">
                 </div>
-                <p>I would love to chat. Email me at daotuananh20121995@gmail.com</p>
 
-                
-            </div>
+                <div class="from-group">
+                    <label name="message">Message:</label>
+                    <textarea id="message" name="message" class="form-control">Type your message here... </textarea>
+                </div>
+
+                <input type="submit" value="Send Message" class="btn btn-success">
+            </form>
         </div>
-    </body>
-</html>
+    </div>
+@endsection     

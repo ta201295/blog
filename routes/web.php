11c72145fd36bee19c716bf070fa514027060038
unknown <daotuananh201295>
2019-11-06 23:21:20 +0700

    blog

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 Route::group(['middleware' => ['web']], function () {
     Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' =>'BlogController@getSingle']);
+    Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
     Route::get('contact', 'PagesController@getContact');
     Route::get('about', 'PagesController@getAbout');
     Route::get('/', 'PagesController@getindex');
